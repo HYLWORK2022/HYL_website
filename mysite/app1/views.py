535e6c66634a34bcf4d166ec21d57140e7cfca1f
HYLWORK2022 <hyl.work.2022@gmail.com>
2023-02-04 01:58:40 +0800

    first subpage app1

--- mysite/app1/views.py
+++ mysite/app1/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    # return HttpResponse("Hello food!")
+    return render(request, 'app1/index.html', {'place_nums': range(6)})
